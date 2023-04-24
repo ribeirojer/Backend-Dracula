6a3e2b5f428285101ca81cb1f8f9bfd742ee6f99
José Eduardo Ribeiro <eduardojerbr@gmail.com>
2023-04-23 21:34:23 -0300

    refactor: edit email transporter

--- src/utils/emailUtils.ts
+++ src/utils/emailUtils.ts
@@ -1,26 +1,61 @@
 import nodemailer from "nodemailer";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const SMTP_HOST = process.env.SMTP_HOST as string;
+const SMTP_PORT = parseInt(process.env.SMTP_PORT as string, 10);
+const SMTP_EMAIL = process.env.SMTP_EMAIL as string;
+const SMTP_PASSWORD = process.env.SMTP_PASSWORD as string;
+
+const transporter = nodemailer.createTransport({
+  host: SMTP_HOST,
+  port: SMTP_PORT,
+  auth: {
+    user: SMTP_EMAIL,
+    pass: SMTP_PASSWORD,
+  },
+} as any);
 
 export async function sendPasswordResetEmail(
   email: string,
   resetLink: string
 ): Promise<void> {
-  const transporter = nodemailer.createTransport({
-    host: "sandbox.smtp.mailtrap.io",
-    port: "2525",
-    auth: {
-      user: "1855e5eb96600e",
-      pass: "53275d8c735b83",
-    },
-  } as any);
+  const passwordResetConfig = {
+    subject: "Redefinição de Senha",
+    html: `
+      <p>Olá,</p>
+      <p>Clique no link abaixo para redefinir sua senha:</p>
+      <a href="${resetLink}" style="display: inline-block; margin-top: 16px; padding: 12px; background-color: #007bff; color: #fff; text-decoration: none;">Redefinir senha</a>
+    `,
+  };
 
-  await transporter.sendMail({
+  const info = await transporter.sendMail({
     from: "recuperacaodesenha@dracula.com",
     to: email,
-    subject: "Password Reset",
-    html: `
-      <p>Hi,</p>
-      <p>Please click the following link to reset your password:</p>
-      <a href="${resetLink}">${resetLink}</a>
-    `,
+    ...passwordResetConfig,
   });
+
+  console.log("Password reset email sent: %s", info.messageId);
+}
+
+export async function sendEmailConfirmation(
+  email: string,
+  confirmationLink: string
+): Promise<void> {
+  const emailConfirmationConfig = {
+    subject: "Confirmação de Email",
+    html: `
+      <p>Olá,</p>
+      <p>Clique no link abaixo para confirmar seu email:</p>
+      <a href="${confirmationLink}" style="display: inline-block; margin-top: 16px; padding: 12px; background-color: #007bff; color: #fff; text-decoration: none;">Confirmar email</a>
+    `,
+  };
+  const info = await transporter.sendMail({
+    from: "confirmacao@dracula.com",
+    to: email,
+    ...emailConfirmationConfig,
+  });
+
+  console.log("Email confirmation sent: %s", info.messageId);
 }
